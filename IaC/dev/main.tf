a29e5d9458bb8b74f7f21751aae7e79ec3e4583b
Tanguy Frémont <fremonttanguy@yahoo.fr>
2025-01-10 11:30:54 +0100

    fix(iac) separate service accounts for cicd/run

--- IaC/dev/main.tf
+++ IaC/dev/main.tf
@@ -5,15 +5,48 @@ resource "google_artifact_registry_repository" "default" {
   project       = var.project_id
 }
 
+resource "google_service_account" "cicd_sa" {
+  account_id   = "cicd-sa"
+  display_name = "CI/CD Service Account"
+  project      = var.project_id
+}
+
+resource "google_project_iam_binding" "artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+
+  members = [
+    "serviceAccount:${google_service_account.cicd_sa.email}"
+  ]
+}
 resource "google_service_account" "cloud_run_sa" {
   account_id   = "cloud-run-sa"
   display_name = "Cloud Run Service Account"
   project      = var.project_id
 }
 
+
 resource "google_project_iam_binding" "artifact_registry_access" {
   role    = "roles/artifactregistry.reader"
   project = var.project_id
+  members = [
+    "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "run_admin" {
+  project = var.project_id
+  role    = "roles/run.admin"
+
+  members = [
+    "serviceAccount:${google_service_account.cloud_run_sa.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+
   members = [
     "serviceAccount:${google_service_account.cloud_run_sa.email}"
   ]
